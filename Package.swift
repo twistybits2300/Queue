a2a889e8a69b8230946bc867f623b1f6fdd47c58
twistybits2300 <twistybits2300@protonmail.com>
2024-05-14 17:04:56 -0700

    Add dependency on LinkedList

--- Package.swift
+++ Package.swift
@@ -11,11 +11,17 @@ let package = Package(
             name: "Queue",
             targets: ["Queue"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/twistybits2300/LinkedList.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Queue"),
+            name: "Queue",
+            dependencies: [
+                .product(name: "LinkedList", package: "LinkedList")
+            ]),
         .testTarget(
             name: "QueueTests",
             dependencies: ["Queue"]),
